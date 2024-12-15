26dcd4428dcc78edb2d606a6d1fef14e8f2820c2
John <johnk.josef@gmail.com>
2024-12-15 23:17:18 +0530

    Update server.c

--- 5. UDP Concurrent Time Server Application/server.c
+++ 5. UDP Concurrent Time Server Application/server.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <time.h>
+#include <sys/time.h>
 
 void main(){
+    int portno;
+    printf("Enter PORT NUMBER: ");
+    scanf("%d",&portno);
     char recvbuf[1024],sendbuf[1024];
     for(int i=0;i<1024;i++){
         recvbuf[i]='\0';
@@ -17,11 +22,14 @@ void main(){
     struct sockaddr_in seraddr,cliaddr;
     int sersoc=socket(AF_INET,SOCK_DGRAM,0);
     seraddr.sin_family=AF_INET;
-    seraddr.sin_port=htons(4950);
-    bind(sersoc, (struct sockaddr *)&seraddr , sizeof(struct sockaddr_in));
+    seraddr.sin_port=htons(portno);
+    if(bind(sersoc, (struct sockaddr *)&seraddr , sizeof(struct sockaddr_in))!=0){
+        printf("\nBINDING ERROR..Try a different PORT\n");
+        exit(0);
+    }
     socklen_t addrlen=sizeof(struct sockaddr_in);
-    int a=recvfrom(sersoc,recvbuf,1024,0,(struct sockaddr *)&cliaddr,&addrlen);
+    recvfrom(sersoc,recvbuf,1024,0,(struct sockaddr *)&cliaddr,&addrlen);
     printf("\nClient Time: %s\n",recvbuf);
-    sendto(sersoc,sendbuf,1024,0,(struct sockaddr *)&cliaddr,addrlen);
+    sendto(sersoc,sendbuf,1024,0,(struct sockaddr *)&cliaddr,sizeof(struct sockaddr_in));
     printf("\nServer time sent to client.\n");
 }
