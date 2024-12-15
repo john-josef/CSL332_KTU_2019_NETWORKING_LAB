1f590b92955a58eec847d48ea40201bd3bdb5766
John <johnk.josef@gmail.com>
2024-12-15 23:18:01 +0530

    Update client.c

--- 5. UDP Concurrent Time Server Application/client.c
+++ 5. UDP Concurrent Time Server Application/client.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-#include <time.h>
+#include <sys/time.h>
 
 void main(){
+    int portno;
+    printf("Enter PORT NUMBER: ");
+    scanf("%d",&portno);
     char recvbuf[1024],sendbuf[1024];
     for(int i=0;i<1024;i++){
         recvbuf[i]='\0';
@@ -17,10 +22,10 @@ void main(){
     struct sockaddr_in seraddr;
     int clisoc=socket(AF_INET,SOCK_DGRAM,0);
     seraddr.sin_family=AF_INET;
-    seraddr.sin_port=htons(4950);
+    seraddr.sin_port=htons(portno);
     socklen_t addrlen=sizeof(struct sockaddr_in);
     sendto(clisoc,sendbuf,1024,0,(struct sockaddr *)&seraddr,sizeof(struct sockaddr_in));
     printf("\nClient time sent to server.\n");
-    int a=recvfrom(clisoc,recvbuf,1024,0,(struct sockaddr *)&seraddr,&addrlen);
+    recvfrom(clisoc,recvbuf,1024,0,(struct sockaddr *)&seraddr,&addrlen);
     printf("\nServer Time: %s\n",recvbuf);
 }
